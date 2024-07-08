8033cbd15bebf1117c0ebb143d409874d0fd4f49
Kye Gomez <swarms_wd@Kyes-MacBook-Pro.local>
2024-07-08 11:58:59 -0700

    [CLEANUP]

--- swarms_memory/__init__.py
+++ swarms_memory/__init__.py
@@ -1,4 +1,4 @@
-from swarms_memory.chroma_db_wrapper import ChromaMemory
+from swarms_memory.chroma_db_wrapper import ChromaDBMemory
 from swarms_memory.pinecone_wrapper import PineconeMemory
 
-__all__ = ["ChromaMemory", "PineconeMemory"]
+__all__ = ["ChromaDBMemory", "PineconeMemory"]
