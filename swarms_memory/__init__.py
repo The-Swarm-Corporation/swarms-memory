5b0cb0ada20328fdca8d957ab3a5d9bec8a7538c
Kye Gomez <swarms_wd@Kyes-MacBook-Pro.local>
2024-08-28 16:41:17 -0400

    [EXAMPLE]

--- swarms_memory/__init__.py
+++
@@ -1,3 +0,0 @@
-from swarms_memory.vector_dbs import *  # noqa: F401, F403
-from swarms_memory.utils import *  # noqa: F401, F403
-from swarms_memory.dbs import *  # noqa: F401, F403
--- swarms_memory/dbs/__init__.py
+++
@@ -1,5 +0,0 @@
-from swarms_memory.dbs.pg import PostgresDB
-from swarms_memory.dbs.sqlite import SQLiteDB
-
-
-__all__ = ["PostgresDB", "SQLiteDB"]
